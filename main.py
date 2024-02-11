b643416cfce1f7134259abd1f74ecdebcacc59e8
AlexxOlsh <toressa599@gmail.com>
2024-02-11 18:19:12 +0300

    Remove log.txt correction main.py level choise

--- main.py
+++ main.py
@@ -40,38 +40,38 @@ if __name__ == "__main__":
         except ValueError:
             print('Введите число! \n')
 
-    print(level)
-    right_answer = randint(0, level_list[level]['max'])
-    print(f"Я загадал число от 0 до {level_list[level]['max']}. Количество попыток: {level_list[level]['count']} \n")
-    write_log("[" + str(datetime.now()) + "][INFO][User][System]: Загадано число: " + str(right_answer))
-    answer = None
-    answer_count = 0
+    if level >= 1 and level <=3 :
+        right_answer = randint(0, level_list[level]['max'])
+        print(f"Я загадал число от 0 до {level_list[level]['max']}. Количество попыток: {level_list[level]['count']} \n")
+        write_log("[" + str(datetime.now()) + "][INFO][User][System]: Загадано число: " + str(right_answer))
+        answer = None
+        answer_count = 0
 
-    while answer != right_answer:
-        try:
-            answer = int(input('Какое число я загадал? \n'))
-            answer_count += 1
-            status = check_number(answer, right_answer)
-            current_dt = datetime.now()
-            check_result = ''
-            if status == 'more':
-                log_list[status].append(answer)
-                check_result = "[" + str(current_dt) + "][INFO][User]: Введено число " + str(answer)
-                print('Не угадали. Число должно быть меньше')
-            elif status == 'less':
-                log_list[status].append(answer)
-                check_result = "[" + str(current_dt) + "][INFO][User]: Введено число " + str(answer)
-                print('Не угадали. Число должно быть больше')
-            elif status == 'equal':
-                check_result = "[" + str(current_dt) + "][INFO][User][System]: Число угадано."
-                print(f'Поздравляю! Вы угадали! Количество попыток: {answer_count}')
+        while answer != right_answer:
+            try:
+                answer = int(input('Какое число я загадал? \n'))
+                answer_count += 1
+                status = check_number(answer, right_answer)
+                current_dt = datetime.now()
+                check_result = ''
+                if status == 'more':
+                    log_list[status].append(answer)
+                    check_result = "[" + str(current_dt) + "][INFO][User]: Введено число " + str(answer)
+                    print('Не угадали. Число должно быть меньше')
+                elif status == 'less':
+                    log_list[status].append(answer)
+                    check_result = "[" + str(current_dt) + "][INFO][User]: Введено число " + str(answer)
+                    print('Не угадали. Число должно быть больше')
+                elif status == 'equal':
+                    check_result = "[" + str(current_dt) + "][INFO][User][System]: Число угадано."
+                    print(f'Поздравляю! Вы угадали! Количество попыток: {answer_count}')
 
-            write_log(check_result)
-            if answer_count == level_list[level]['count']:
-                print('Вы проиграли. Количество попыток закончилось')
-                break
-        except ValueError:
-            print('Введите число! \n')
+                write_log(check_result)
+                if answer_count == level_list[level]['count']:
+                    print('Вы проиграли. Количество попыток закончилось')
+                    break
+            except ValueError:
+                print('Введите число! \n')
 
-    write_log('Попыток: ' + str(answer_count))
-    print(log_list)
+        write_log('Попыток: ' + str(answer_count))
+        print(log_list)
